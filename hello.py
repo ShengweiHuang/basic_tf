b20f10229d8e1359b4cec6bca9a3917723aca9ab
ShengweiHuang <huang6275@gmail.com>
2017-11-20 15:28:01 +0700

    v1.0

---
+++ hello.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+def main():
+	basic_cal()
+
+def hello():
+	hello = tf.constant("hello, tensorflow")
+	sess = tf.Session()
+	print(sess.run(hello))
+
+def basic_cal():
+	a = tf.constant(5.0)
+	b = tf.constant(6.0)
+	c = a * b
+	sess = tf.Session()
+	print(sess.run(c))
+
+if __name__ == "__main__":
+	main()
