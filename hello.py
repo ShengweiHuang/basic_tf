3c8690133fcbe2ae65348956defdc5066a53dfca
ShengweiHuang <huang6275@gmail.com>
2017-11-20 17:52:35 +0700

    v1.1

--- hello.py
+++ hello.py
@@ -1,7 +1,5 @@
 import tensorflow as tf
-
-def main():
-	basic_cal()
+import numpy as np
 
 def hello():
 	hello = tf.constant("hello, tensorflow")
@@ -16,7 +14,39 @@ def basic_cal():
 	print(sess.run(c))
 
 def random_float():
-	return
+	# create 100 random data, value between 0 to 1
+	x_data = np.random.rand(100).astype(np.float32)
+	# define learning function
+	# weight = 0.1
+	# biases = 0.3
+	y_data = x_data * 0.1 + 0.3
+	# create tensorflow structure
+	# set weights range and init
+	weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+	biases = tf.Variable(tf.zeros([1]))
+	# set tensorflow learning function
+	y = weights * x_data + biases
+	# set loss rule
+	loss = tf.reduce_mean(tf.square(y - y_data))
+	# set learning oprimizer, learning rate = 0.5
+	optimizer = tf.train.GradientDescentOptimizer(0.5)
+	# training rule = minimize loss
+	train = optimizer.minimize(loss)
+	# init variable
+	init = tf.initialize_all_variables()
+	# end create tensorflow structure
+	# start training
+	sess = tf.Session()
+	# init session
+	sess.run(init)
+	'''
+	using for loop update weight 201 times
+	print weight every 20 times
+	'''
+	for step in range(0, 201):
+		sess.run(train)
+		if step % 20 == 0:
+			print(step, sess.run(weights), sess.run(biases))
 
 if __name__ == "__main__":
-	main()
+	random_float()
